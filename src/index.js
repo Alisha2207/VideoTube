e8b57ca3d9f8cfb1a3e96dc10264afc681c1a78d
ChaudharyAlisha <chaudharyalisha2005@gmail.com>
2024-12-26 18:18:08 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require ('dotenv').config()
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB CONNECTION FAILED", err);
+  });
+
+/*
+import express from "express"
+const app= express()
+
+(async()=>{
+    try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (error)=>{
+        console.log("ERRRR:", error );
+        throw error
+    })
+
+    app.listen(process.env.PORT, ()=>{
+        console.log(`app is listening on port ${process.env.PORT}`);
+    })
+
+    } catch (error) {
+        console.error("ERROR", error);
+        throw err
+    }
+})()*/
